34aff9648484ba5eb0cec73d55300eb0f5c44036
Jim <mckay74@gmail.com>
2014-08-30 20:05:17 -0400

    Destroy

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
   belongs_to :user  #for comment checkpoint
+
+  validates :body, length: { minimum: 5 }, presence: true
+  validates :user, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Bloccit::Application.routes.draw do
 
   resources :topics do 
     resources :posts, except: [:index] do
-      resources :comments, only: [:create]   #comments checkpoint
+      resources :comments, only: [:create, :destroy]   #comments checkpoint
     end
   end
 
