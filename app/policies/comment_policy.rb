f02a82a69c26c930021a661ae95830a53ab46321
Jim <mckay74@gmail.com>
2014-08-31 16:15:46 -0400

    Added comment policy

--- app/policies/comment_policy.rb
+++ app/policies/comment_policy.rb
@@ -1,4 +1,9 @@
 class CommentPolicy < ApplicationPolicy
+
+  def create?
+    user.present? && (record.user == user || user.role?(:admin))
+  end
+
   def destroy?
     user.present? && (record.user == user || user.role?(:admin) || user.role?(:moderator))
   end
