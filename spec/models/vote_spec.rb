0dd20fbbd38f69d282729eebb682474c1bf32314
Jim <mckay74@gmail.com>
2014-09-06 17:53:41 -0400

    Mocking with rspec

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,16 +1,30 @@
 require 'rails_helper'
 
-describe Vote do
-  describe "validations" do 
+describe Vote do 
 
-    before do
-      @v2 = Vote.new( value: -1)
-    end
+  include TestFactories
+  
+  describe "validations" do
+    describe "value validation" do
+      it "only allows -1 or 1 as values" do
+        v = Vote.new(value: 1)
+        expect(v.valid?).to eq(true)
 
-    describe "value validation" do 
-      it "only allows -1 as value" do
-        expect( @v2.value? ).to eq(true)
+        v2 = Vote.new(value: -1)
+        expect(v2.valid?).to eq(true)
+
+        bad_v = Vote.new(value: 2)
+        expect(bad_v.valid?).to eq(false)
       end
+    end
+  end
+
+  describe 'after_save' do 
+    it "calls 'Post#update_rank' after save" do
+      post = associated_post
+      vote = Vote.new(value: 1, post: post)
+      expect(post).to receive(:update_rank)
+      vote.save
     end
   end
 end
